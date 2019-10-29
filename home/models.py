7cc21ddd4343a8b77c10ea779e64b10f24188028
Chris Wedgwood <wedgemail@gmail.com>
2019-10-29 07:57:21 +0000

    add context

--- home/models.py
+++ home/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+from meetings.models import Meeting
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
     pass
+
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        # Add extra variables and return the updated context
+        context['meetings'] = Meeting.objects.all()
+        return context
