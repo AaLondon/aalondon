541fdc3aebaaf69dd6facfc47a583bf44dea5731
Chris Wedgwood <wedgemail@gmail.com>
2019-10-30 22:40:58 +0000

    get meetings

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from meetings.models import Meeting
 from wagtail.core.models import Page
 from datetime import datetime,timedelta
+from django.db.models import Q
 
 
 class HomePage(Page):
@@ -12,10 +13,18 @@ class HomePage(Page):
 
         # Add extra variables and return the updated context
         now = datetime.now() 
-        day_name_now = now.strftime("%A")
+        day_name_today = now.strftime("%A")
         tomorrow = now + timedelta(days=1) 
         day_name_tomorrow = tomorrow.strftime("%A")
-        meetings = Meeting.objects.filter((Q(day=day_name_now) & Q(time__gte=now.time()))|(Q(day=day_name_tomorrow) & Q(time__lte=now.time())))
-        context['meetings'] = meetings
+        #meetings = Meeting.objects.filter((Q(day=day_name_now) & Q(time__gte=now.time()))|(Q(day=day_name_tomorrow) & Q(time__lte=now.time())))
+        
+        meetings_today = Meeting.objects.filter((Q(day=day_name_today) & Q(time__gte=now.time()))).order_by('time')
+        meetings_tomorrow = Meeting.objects.filter((Q(day=day_name_tomorrow) & Q(time__lte=now.time()))).order_by('time')
+        
+        context['meetings_today'] = meetings_today 
+        context['meetings_tomorrow'] = meetings_tomorrow 
+        context['day_name_tomorrow'] = day_name_tomorrow
+        context['day_name_today'] = day_name_today
+
 
         return context
--- meetings/management/commands/get_meetings.py
+++ meetings/management/commands/get_meetings.py
@@ -22,26 +22,27 @@ class Command(BaseCommand):
         #iterate over intergroups and import meetings into meeting model
 
         print('get meetings')
-        page = requests.get('https://www.alcoholics-anonymous.org.uk/markers.do?ig=23') 
-        soup = BeautifulSoup(page.text, 'html.parser') 
-        meetings=soup.find_all('marker')
-        for meeting in meetings:
-            
-            
-            address = meeting.get('address')
-            code = meeting.get('code')
-            day = meeting.get('day')
-            hearing = meeting.get('hearing')
-            lat = meeting.get('lat')
-            lng = meeting.get('lng')
-            postcode = meeting.get('postcode')
-            slat = meeting.get('slat')
-            slng = meeting.get('slng')
-            time = meeting.get('time')
-            title = meeting.get('title')
-            wheelchair = meeting.get('wheelchair')
-            
-            time = time.replace(".",":")
-            new_meeting = Meeting.objects.get_or_create(address=address,code=code,day=day,hearing=hearing,lat=lat,lng=lng,postcode=postcode,time=time,title=title,wheelchair=wheelchair)
+        ids = [117,36,123,124,118,51,64,63,62,119,120,75,55,122,121,77,42]    
+        for id in ids:
+            page = requests.get(f'https://www.alcoholics-anonymous.org.uk/markers.do?ig={id}') 
+            soup = BeautifulSoup(page.text, 'html.parser') 
+            meetings=soup.find_all('marker')
+            for meeting in meetings:
+                
+                print(meeting)
+                address = meeting.get('address')
+                code = meeting.get('code')
+                day = meeting.get('day')
+                hearing = meeting.get('hearing')
+                lat = meeting.get('lat') or None
+                lng = meeting.get('lng') or None
+                postcode = meeting.get('postcode')
+                slat = meeting.get('slat')
+                slng = meeting.get('slng')
+                time = meeting.get('time')
+                title = meeting.get('title')
+                wheelchair = meeting.get('wheelchair')
+                time = time.replace(".",":")
+                new_meeting = Meeting.objects.get_or_create(address=address,code=code,day=day,hearing=hearing,lat=lat,lng=lng,postcode=postcode,time=time,title=title,wheelchair=wheelchair)
 
 
