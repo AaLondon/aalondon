e1c041b649d62e8dd971d0ff4a73553add9b8424
Chris Wedgwood <wedgemail@gmail.com>
2019-11-02 13:30:26 +0000

    add story page

--- home/models.py
+++ home/models.py
@@ -4,6 +4,10 @@ from wagtail.core.models import Page
 from datetime import datetime,timedelta
 from django.db.models import Q
 from django.core import serializers
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 
 class HomePage(Page):
@@ -33,3 +37,20 @@ class HomePage(Page):
 
 
         return context
+
+
+class StoryPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
