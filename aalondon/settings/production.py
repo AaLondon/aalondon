d8c028da4c7567e3ba713b48194b7ac47d6c77f4
Chris Wedgwood <wedgemail@gmail.com>
2019-11-03 10:21:03 +0000

    db - F

--- aalondon/settings/production.py
+++ aalondon/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 from .base import env
-DEBUG = True
+
 
 
 
@@ -13,3 +13,5 @@ except ImportError:
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['206.189.30.197'] 
+
+DEBUG = False
