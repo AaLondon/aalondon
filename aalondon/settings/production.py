84f70d8960e4410d1a50027090b7d71901789faa
Chris Wedgwood <wedgemail@gmail.com>
2019-11-03 13:27:55 +0000

    removing redundant css

--- aalondon/settings/production.py
+++ aalondon/settings/production.py
@@ -11,7 +11,38 @@ except ImportError:
 
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-
+DEBUG = False
 ALLOWED_HOSTS = ['206.189.30.197'] 
 
-DEBUG = False
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'site_errors.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
