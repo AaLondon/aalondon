3b178ba9acbaa7d3a3b5a261e737712a915067e0
Chris Wedgwood <wedgemail@gmail.com>
2019-11-03 13:50:21 +0000

    logging

--- aalondon/settings/production.py
+++ aalondon/settings/production.py
@@ -17,32 +17,18 @@ ALLOWED_HOSTS = ['206.189.30.197']
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
     'handlers': {
         'file': {
-            'level': 'ERROR',
+            'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': 'site_errors.log',
-            'formatter': 'verbose'
+            'filename': '/home/deployer/debug.log',
         },
     },
     'loggers': {
         'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
             'handlers': ['file'],
             'level': 'DEBUG',
+            'propagate': True,
         },
-    }
+    },
 }
