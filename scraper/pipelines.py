7e84fbaaa54a120d3bc57b73e759fae18b410050
Chris Wedgwood <wedgemail@gmail.com>
2019-12-24 07:58:54 +0000

    test

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -3,6 +3,8 @@ from meetings.models import Meeting
 
 class MeetingPipeline(object):
       def process_item(self, item, spider):
-          print('yeboyes')
+          
           meeting, created = Meeting.objects.update_or_create(code=item['code'],defaults=item)
+          print(meeting)
+          print(created)
           return meeting
