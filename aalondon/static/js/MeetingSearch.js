06afd47de7a8373ec2a17d7952082952d9432a44
Chris Wedgwood <wedgemail@gmail.com>
2020-02-11 12:11:16 +0000

    promise geo

--- aalondon/static/js/MeetingSearch.js
+++ aalondon/static/js/MeetingSearch.js
@@ -21,58 +21,77 @@ class MeetingSearch extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.onPageChanged = this.onPageChanged.bind(this);
     this.onDayChange = this.onDayChange.bind(this);
- 
 
 
-    this.state = { totalMeetings: 0, currentMeetings: [], currentPage: 1, totalPages: null, day: "Now",showSpinner: 1, intergroup: '', 
-    clientLng: null, clientLat: null
-     };
+
+    this.state = {
+      totalMeetings: 0, currentMeetings: [], currentPage: 1, totalPages: null, day: "Now", showSpinner: 1, intergroup: '',
+      clientLng: null, clientLat: null,showPostcode:0
+    };
   }
 
-  
+
 
   getPosition() {
     // Simple wrapper
     return new Promise((res, rej) => {
-        navigator.geolocation.getCurrentPosition(res, rej);
+      navigator.geolocation.getCurrentPosition(res, rej);
     });
   }
 
 
   componentDidMount() {
-    
+
     let day = new Date().toLocaleString('en-us', { weekday: 'long' });
-    
-    let queryString = 
+
+    let queryString = "";
+    let lat;
+    let lng;
     this.getPosition().then((position) => {
       // successMessage is whatever we passed in the resolve(...) function above.
       // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
       //
-      let lat = position.coords['latitude']
-      let lng = position.coords['latitude']
-      return `/api/meetingsearch/?day=${day}&now=1&clientLat=${lat}&clientLng=${lng}` 
-      
+      console.log("A");
+      lat = position.coords['latitude']
+      lng = position.coords['longitude']
+      this.setState({clientLat:lat,clientLng:lng});
+      queryString = `/api/meetingsearch/?day=${day}&now=1&clientLat=${lat}&clientLng=${lng}`
+      return queryString
+
+    }
+      ,
+      () => {
+        //TODO WHEN GEO CANT BE FOUNG
+        console.log("B");
+
+        return `/api/meetingsearch/?day=${day}&now=1`
+
+      }
+
+    ).then((queryString) => {
+      console.log("C");
+
+      console.log(queryString);
+      axios.get(queryString)
+
+        .then(response => {
+          const totalMeetings = response.data.count;
+          const currentMeetings = response.data.results;
+          const totalPages = response.data.count / 10;
+
+          this.setState({ totalMeetings: totalMeetings, currentMeetings: currentMeetings, currentPage: 1, totalPages: totalPages, showSpinner: 0 });
+          this.setState({ currentPage: 1 });
+
+        });
     });
 
-    
-    console.log('xxx');
-    console.log(queryString);
-    console.log('xxx');
-    
-    this.setState({ currentPage: 1});
 
-   console.log(queryString);
-    axios.get(queryString)
 
-      .then(response => {
-        const totalMeetings = response.data.count;
-        const currentMeetings = response.data.results;
-        const totalPages = response.data.count / 10;
 
-        this.setState({ totalMeetings: totalMeetings, currentMeetings: currentMeetings, currentPage: 1, totalPages: totalPages,showSpinner: 0  });
-      });
-    }
-  
+
+
+  }
+
   getQueryString() {
     let intergroup = this.state.intergroup;
     let day = this.state.day;
@@ -83,7 +102,7 @@ class MeetingSearch extends Component {
 
 
   onPageChanged = data => {
-   // console.log('onPageChanged');
+    // console.log('onPageChanged');
 
 
     const { currentPage, totalPages, } = data;
@@ -116,7 +135,7 @@ class MeetingSearch extends Component {
   }
 
   onDayChange = data => {
-    
+
     console.log('onDayCHange');
     console.log(data);
     let intergroup = this.state.intergroup;
@@ -133,17 +152,19 @@ class MeetingSearch extends Component {
       now = 1;
     }
 
-    this.setState({ day: data,showSpinner: 1 });
+    this.setState({ day: data, showSpinner: 1 });
     
-    let queryString = `/api/meetingsearch/?intergroup=${intergroup}&day=${query_day}&now=${now}&clientLat=${this.state.clientLat}&clientLng=${this.state.clientLng}`;
-
+    let queryString = `/api/meetingsearch/?intergroup=${intergroup}&day=${query_day}&now=${now}`;
+    if(this.state.clientLat){
+      queryString = `/api/meetingsearch/?intergroup=${intergroup}&day=${query_day}&now=${now}&clientLat=${this.state.clientLat}&clientLng=${this.state.clientLng}`;
+    }
     axios.get(queryString)
       .then(response => {
         const totalMeetings = response.data.count;
         const currentMeetings = response.data.results;
         const totalPages = response.data.count / 10;
-      
-        this.setState({ totalMeetings, currentMeetings, currentPage, totalPages ,showSpinner: 0});
+
+        this.setState({ totalMeetings, currentMeetings, currentPage, totalPages, showSpinner: 0 });
       });
 
 
@@ -153,23 +174,23 @@ class MeetingSearch extends Component {
 
 
   render() {
-  
-    const { totalMeetings, currentMeetings, currentPage, totalPages, day,showSpinner } = this.state;
-  
+
+    const { totalMeetings, currentMeetings, currentPage, totalPages, day, showSpinner } = this.state;
+
     console.log('render');
     console.log(day);
-    if (showSpinner === 1 )
-     return( <Container>
-     <MeetingSearchForm value={this.state.value} onInputChange={this.handleInputChange} onDayChange={this.onDayChange} onIntergroupChange={this.onIntergroupChange} day={this.state.day} intergroup={this.state.intergroup} />
+    if (showSpinner === 1)
+      return (<Container>
+        <MeetingSearchForm value={this.state.value} onInputChange={this.handleInputChange} onDayChange={this.onDayChange} onIntergroupChange={this.onIntergroupChange} day={this.state.day} intergroup={this.state.intergroup} />
         <Row className="justify-content-center"><Col xs={2}> <Spinner size="lg" animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-        </Spinner></Col></Row> 
-     
+          <span className="sr-only">Loading...</span>
+        </Spinner></Col></Row>
 
-    </Container>)
-    
-    
-    
+
+      </Container>)
+
+
+
     if (totalMeetings === 0) return (
 
       <div>
@@ -178,7 +199,7 @@ class MeetingSearch extends Component {
           {/* Stack the columns on mobile by making one full-width and the other half-width */}
           <MeetingSearchForm value={day} onInputChange={this.handleInputChange} onDayChange={this.onDayChange} onIntergroupChange={this.onIntergroupChange} day={this.state.day} intergroup={this.state.intergroup} />
           <div>NO MEETINGS FOR THE REST OF THE DAY PLEASE CHECK TOMORROW</div>
-         
+
 
         </Container>
 
@@ -196,7 +217,7 @@ class MeetingSearch extends Component {
       <div>
 
         <Container>
-       
+
           {/* Stack the columns on mobile by making one full-width and the other half-width */}
           <MeetingSearchForm value={this.state.day} onInputChange={this.handleInputChange} onDayChange={this.onDayChange} onIntergroupChange={this.onIntergroupChange} day={this.state.day} intergroup={this.state.intergroup} />
           <MeetingTableData key={firstCode} currentMeetings={this.state.currentMeetings} clientLat={this.state.clientLat} />
