d754ebfa1919ef2fc3bc39717bbd33ff6601ee6e
Chris Wedgwood <wedgemail@gmail.com>
2020-02-20 07:42:10 +0000

    get value

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,9 +1,14 @@
 # context_processors.py
+import environ
 from service.models import ServicePage
+
+env = environ.Env()
+GA_TRACKING_ID = env('GA_TRACKING_ID',default='None')
 
 def servicepages(request):
     servicepages = ServicePage.objects.all()
     return {
        "servicepages": servicepages,
-       "ga_tracking_id": 'UA-158763652-1'
+       "ga_tracking_id": GA_TRACKING_ID 
+
     }
