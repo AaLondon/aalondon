f77a895840bea98d24817b34299d854151d0a079
Chris Wedgwood <chriswedgwood@tfl.gov.uk>
2020-02-21 16:11:23 +0000

    css changes

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register)
-from .models import RecurringEventChild
+from .models import RecurringEventChild,EventType
 
 class RecurringEventChildAdmin(ModelAdmin):
     model = RecurringEventChild
@@ -9,5 +9,11 @@ class RecurringEventChildAdmin(ModelAdmin):
     ordering = ['date']
     menu_label = 'Recurring Events'
 
+class EventTypeAdmin(ModelAdmin):
+    model = EventType
+    list_display = ('value', )
+    
+
+modeladmin_register(EventTypeAdmin)
 
 modeladmin_register(RecurringEventChildAdmin)
