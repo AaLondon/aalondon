e348bab3c10e956ed2132c1e0f2925d368478f61
Chris Wedgwood <wedgemail@gmail.com>
2020-03-11 07:16:45 +0000

    fix collect_static

--- aalondon/settings/base.py
+++ aalondon/settings/base.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/2.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
