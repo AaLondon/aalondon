52344489aea788b790f85f7be4fa80a69fa5d2f5
Tintinnabulate <tintinn@abulate>
2020-07-22 19:55:14 +0100

    Add unit tests to test /api/* endpoints #125
    * Makefile
    Add; two rules, all: rebuild and run docker, test: run unittests
    
    * api/tests.py
    API unit tests for all api endpoints
    
    * api/urls.py
    Remove unused URL /meetings2
    
    * api/views.py
    Remove unused view /meetings2 mapped to MeetingsList3

--- api/tests.py
+++ api/tests.py
@@ -1,20 +1,18 @@
 from django.test import TestCase
 
 class TestURLSuccess(TestCase):
-    def test_visit_home(self):
-        response = self.client.get('/')
+    """
+    Just test that each api endpoint returns 200
+    """
+    def test_visit_api_root(self):
+        response = self.client.get('/api/')
         assert response.status_code == 200
-    def test_visit_meetings2(self):
-        response = self.client.get('/meetings2/')
-        print (response)
-        # [Tintinnabulate 2020-07-22] XXX:
-        # `response.status_code == 404` because meetingslist2 goes nowhere!
-        # I think you might mean meetingslist3...
+    def test_visit_meetings(self):
+        response = self.client.get('/api/meetings/')
         assert response.status_code == 200
     def test_visit_meetingsearch(self):
-        response = self.client.get('/meetingsearch/')
+        response = self.client.get('/api/meetingsearch/')
         assert response.status_code == 200
     def test_visit_onlinemeetingsearch(self):
-        response = self.client.get('/onlinemeetingsearch/')
+        response = self.client.get('/api/onlinemeetingsearch/')
         assert response.status_code == 200
-    # [Tintinnabulate 2020-07-22] TODO: test rest_framework.urls
