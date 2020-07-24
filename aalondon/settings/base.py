0ad3ca5b7c3a351c1f17e55253fd867d715a85cc
Chris Wedgwood <wedgemail@gmail.com>
2020-07-24 23:11:16 +0100

    try database_url

--- aalondon/settings/base.py
+++ aalondon/settings/base.py
@@ -203,14 +203,3 @@ REST_FRAMEWORK = {
 }
 
 
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'aalondon',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
