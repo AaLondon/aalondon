d92ff051e842b9162f18a9a09893c2e80802c41d
Chris Wedgwood <wedgemail@gmail.com>
2021-03-06 19:48:12 +0000

    remove GA and add chatbot options

--- aalondon/static/js/ChatBot.js
+++ aalondon/static/js/ChatBot.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import ChatBot from "react-simple-chatbot";
 import "../css/bot.css";
 
+async function GoToUrl(props) {
+  let url = props.url
+  let label = props.label
+  window.open(url, "_self");
+  return label
+}
+
 
 const steps = [
   {
@@ -20,20 +27,9 @@ const steps = [
   },
   {
     id: "3",
-    component: (
-      <div>
-        {" "}
-        This is a example component with <a href="some_link">link_text</a>{" "}
-      </div>
-    ),
-    asMessage: true,
-    end: true,
-  },
-  {
-    id: "4",
     message:
-      "Great. Let’s help you to get what you need from our AA-London site.",
-    trigger: "5",
+      "Great. Here are some options for you",
+    trigger: "13",
   },
   {
     id: "5",
@@ -49,36 +45,126 @@ const steps = [
     ],
   },
   {
-      id:"6",
-      message:
+    id: "6",
+    message:
       "Ok. How about these?",
     trigger: "7",
 
   },
   {
-      id:"7",
-      options: [
-        { value: 1, label: "I might have a drinking problem and want to speak with someone.", trigger: "8" },
-        { value: 2, label: "Existing Member", trigger: "9" },
-      ],
+    id: "7",
+    options: [
+      { value: 1, label: "I want to speak to someone", trigger: "8" },
+      { value: 2, label: "I'm helping someone else", trigger: "10" },
+    ],
   },
   {
-      id:"8",
-      message:"Sure. You can do that in different ways...",
-      trigger:"9"
+    id: "8",
+    message: "Sure. You can do that in different ways...",
+    trigger: "9"
 
   },
   {
-      id:"9",
-      options: [
-        { value: 1, label: "Live Chat", trigger: "8" },
-        { value: 2, label: "Send AA an email", trigger: "9" },
-        { value: 3, label: "Call the AA helpline 0800 9177 650", trigger: "9" },
-        
+    id: "9",
+    options: [
+      { value: 1, label: "Live Chat(opens in a new window)", trigger: "8" },
+      { value: 2, label: "Send AA an email", trigger: "9" },
+      { value: 3, label: "Call the AA helpline 0800 9177 650", trigger: "12" },
 
-      ],
 
-  }
+    ],
+
+  },
+  {
+    id: "10",
+    message: "Sure. Here are some options...",
+    trigger: "11"
+
+  },
+  {
+    id: "11",
+    options: [
+      
+      { value: 1, label: "AA Informational Videos" },
+      { value: 2, label: "Call the AA helpline 0800 9177 650" },
+
+
+    ],
+
+  },
+  {
+    id: '12',
+    component: <GoToUrl url={'/meetingsearch'} label={'Taking you to our meeting finder'} />,
+    asMessage: true,
+    end: true
+  },
+  {
+    id: "4",
+    message:
+      "Great. Let’s help you to get what you need from our AA-London site.",
+    trigger: "13",
+  },
+  {
+    id: "13",
+    options: [
+      { value: 1, label: "Find a meeting", trigger: "14" },
+      { value: 2, label: "Learn about Service", trigger: "15" },
+      { value: 3, label: "Practicing Tradition 7", trigger: "16" },
+      { value: 4, label: "Upcoming Events", trigger: "17" },
+      { value: 5, label: "Update my meeting details", trigger: "18" },
+      { value: 6, label: "I'm helping someone else", trigger: "19" },
+    ],
+  },
+  {
+    id: '14',
+    component: <GoToUrl url={'/meetingsearch'} label={'Taking you to our meeting finder...'} />,
+    asMessage: true,
+    end: true
+  },
+  {
+    id: '15',
+    component: <GoToUrl url={'/service'} label={'Taking you to our Service page...'} />,
+    asMessage: true,
+    end: true
+  },
+  {
+    id: '16',
+    component: <GoToUrl url={'/resources/tradition-7-online/'} label={'Taking you to our Tradition 7 info....'} />,
+    asMessage: true,
+    end: true
+  },
+  {
+    id: '17',
+    component: <GoToUrl url={'/events'} label={'Taking you to our Events....'} />,
+    asMessage: true,
+    end: true
+  },
+  {
+    id: '18',
+    component: <GoToUrl url={'/update'} label={'Lets get your meeting updated....'} />,
+    asMessage: true,
+    end: true
+  },
+  {
+    id: "19",
+    message: "Sure. Here are some options...",
+    trigger: "20"
+
+  },
+  {
+    id: "20",
+    options: [
+      
+      { value: 1, label: "AA Literature" },
+      { value: 2, label: "AA Infomational Videos" },
+      { value: 3, label: "Phone the AA UK helpline 0800 9177 650" },
+
+
+    ],
+
+  },
+  
+
 ];
 
 ReactDOM.render(
