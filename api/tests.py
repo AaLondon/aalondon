46fbbd59f43913105b0461ecbec9341e0c85a8e1
Kegan Overberg <keganoverberg@Kegans-MacBook-Air.local>
2023-01-02 23:25:19 +0200

    added meeting type to meeting search tests

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from meetings.models import Meeting
@@ -31,11 +32,18 @@ class TestURLSuccess(TestCase):
 class TestMeetingAPI(APITestCase):
 
     def setUp(self):
+        self.meeting_types = ["F2F", "ONL", "HYB"] 
         self.meeting_count = 50
+
         # create meeting test data.
         for _ in range(self.meeting_count):
-            Meeting(title=f"Meeting 0{_}", time=datetime.time(10,30), published=True).save()
-            
+            Meeting(
+                title=f"Meeting 0{_}",
+                time=datetime.time(10,30),
+                published=True,
+                type=random.choice(self.meeting_types)).save()
+        
+        # api urls.
         self.api_meetings_url = "/api/meetings/"
         self.api_meetingsearch_url = "/api/meetingsearch/"
     
@@ -45,9 +53,30 @@ class TestMeetingAPI(APITestCase):
         self.assertEquals(count, self.meeting_count)
 
     def test_api_meetingsearch(self):
-        test_search = "Meeting 028"
+        """
+            NOTE:
+                - test `/api/meetingsearch/` api by searching random meeting
+                  titles and types.
+        """
+        num = random.choice([_ for _ in range(self.meeting_count)])
+        test_search = f"Meeting 0{num}"
+        meeting_type = random.choice(self.meeting_types) 
         response = self.client.get(
-            self.api_meetingsearch_url + "?search=meeting%20028",
+            self.api_meetingsearch_url + f"?search=meeting%200{num}&type={meeting_type}",
             format="json") # call `/api/meetingsearch/` api.
-        results = response.json()["results"][0]
-        self.assertEqual(results["title"], test_search)
+        
+        # get returned meeting results.
+        meeting_data = response.json()
+
+        # if meeting_count is greater than 0, results were found.
+        if int(meeting_data["count"]) > 0:
+            # get meeting title and type from json data.
+            search_title = meeting_data["results"][0]["title"]
+            search_type = meeting_data["results"][0]["type"]
+
+            # test if search returned correct results.
+            if (search_title == test_search) and (search_type == meeting_type):
+                self.assertEqual(search_title, test_search)
+                self.assertEqual(search_type, meeting_type)
+        else:
+            self.assertEquals(meeting_data["count"], 0)
