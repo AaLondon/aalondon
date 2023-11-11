631ade8cee96f9212b6a366bf83b628871a10964
Chris Wedgwood <wedgemail@gmail.com>
2023-11-11 12:48:53 +0000

    django 41 wagtail 4

--- service/models.py
+++ service/models.py
@@ -8,7 +8,7 @@ from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
-from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.edit_handlers import FieldPanel
 from wagtail.search import index
 import datetime
 
