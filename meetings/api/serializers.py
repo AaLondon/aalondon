66844c4c1bfc43fcfc6a1efff63353288e1bcf8e
Chris Wedgwood <wedgemail@gmail.com>
2024-10-12 20:22:17 +0100

    fix 2

--- meetings/api/serializers.py
+++ meetings/api/serializers.py
@@ -84,12 +84,12 @@ class MeetingSerializer(serializers.ModelSerializer):
             meeting_sub_type, created = MeetingSubType.objects.get_or_create(value=sub_type["value"])
             meeting.sub_types.add(meeting_sub_type)
 
-        # mail.send_mail(
-        #     f"aa-london.com | {title} Email Confirmation.",
-        #     f"Hi\n\nSo that we can publish your meeting please confirm by clicking the link below.\n\n{confirmation_link(meeting.pk, title, self.context.get('request'))}\n\nIn fellowship,\nAA London Website Team",
-        #     'info@aa-london.com',
-        #     [email]
-        # )
+        mail.send_mail(
+            f"aa-london.com | {title} Email Confirmation.",
+            f"Hi\n\nSo that we can publish your meeting please confirm by clicking the link below.\n\n{confirmation_link(meeting.pk, title, self.context.get('request'))}\n\nIn fellowship,\nAA London Website Team",
+            'info@aa-london.com',
+            [email]
+        )
         return meeting
 
     
